1b84de653d7a05eb7a59eb2995ec150847f7f9d3
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-05-29 06:33:57 +0000

    add inductive minier algo and convert process tree to petri net

--- algorithms/inductive_miner.py
+++ algorithms/inductive_miner.py
@@ -1,10 +1,17 @@
 from fastapi import UploadFile, HTTPException
+from pm4py.objects.log.obj import EventLog
+from pm4py.objects.petri_net.obj import PetriNet, Marking
+from pm4py.algo.discovery.inductive import algorithm as inductive_miner_algorithm
+from pm4py.objects.conversion.process_tree import converter as pt_converter
 
 
-def inductive_miner():
+def inductive_miner(eventlog: EventLog):
+    process_tree = inductive_miner_algorithm.apply(eventlog)
+    net, im, fm = pt_converter.apply(process_tree)
+
     #just return sth to test
     return {
-        "algorithm": "alpha_miner",
+        "algorithm": "inductive_miner",
         "places": len(net.places),
         "transitions": len(net.transitions),
         "arcs": len(net.arcs)
