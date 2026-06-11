126eb323847c1dd6727248fd613d188e02feec29
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-06-11 09:15:35 +0000

    name change; add files for export petrinet and bpmn, add helper to resolve uuids of metamodels and its classes

--- algorithms/alpha_miner.py
+++ algorithms/alpha_miner.py
@@ -3,12 +3,19 @@ from pm4py.objects.log.obj import EventLog
 from pm4py.objects.petri_net.obj import PetriNet, Marking
 from pm4py.algo.discovery.alpha import algorithm as alpha_miner_algorithm
 
+#alpha miner can be executed as classic or plus version; return a net, marking and final_marking
+#reference pm4py package: https://pm4py-source.readthedocs.io/en/stable/pm4py.algo.discovery.alpha.html last visited 11.06.26
 
 def alpha_miner(eventlog: EventLog):
-    net: PetriNet
+    #initalize variables for return
+    net: PetriNet 
     im: Marking
     fm: Marking
+
+    #apply alpha algorithm
     net, im, fm = alpha_miner_algorithm.apply(eventlog)
+
+    #call converter to save PetriNet to MM-AR
 
     #just return sth to test
     return {
---
+++ exporters/petri_net.py
@@ -0,0 +1,5 @@
+
+
+#saves Petri Net to MM-AR
+
+#use uuid resolver to get uuid of Petri net
