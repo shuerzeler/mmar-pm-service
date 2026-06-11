13e6d0855cfa7bfbcc734ac70575bc837f87a27a
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-06-11 09:27:41 +0000

    add skelleton for uuid resolver function

--- exporters/petri_net.py
+++ exporters/petri_net.py
@@ -1,5 +1,10 @@
 
 
-#saves Petri Net to MM-AR
+#saves Petri Net to MM-AR; takes as input a net object, marking and final marking
+#??? do i really need final marking? because transitioning could/should be already impemlelmtned for the metamodel
 
-#use uuid resolver to get uuid of Petri net
+#use uuid resolver to get uuid of Petri net, transition, places and marking
+
+#create net and inital marking
+
+#return OK or throw error
---
+++ helpers/uuid_resolver.py
@@ -0,0 +1,27 @@
+
+import requests
+import os
+
+#server URL for backend server
+BASE_URL = "http://mmar-server:8000"
+
+#----------------------------------Resolve Petri Net and its components----------------------------
+def getPetriNetUUID():
+
+
+
+def getTransitionUUID():
+    
+
+
+def getPlacesUUID():
+    
+
+
+def getMarkingUUID():
+    
+
+
+#----------------------------------Resolve BPMN and its components----------------------------
+def getBPMNUUID():
+
