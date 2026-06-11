63d4ca4ddfc7c04d67b52fbf744fd9350cadb6aa
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-06-11 15:15:47 +0000

    add uuid resolve for petri-net

--- helpers/uuid_resolver.py
+++ helpers/uuid_resolver.py
@@ -1,26 +1,48 @@
-
 import requests
 import os
 
-#server URL for backend server
+#server URL for backend serverand login
 BASE_URL = "http://mmar-server:8000"
+USERNAME = os.getenv("MMAR_USERNAME", "admin")
+PASSWORD = os.getenv("MMAR_PASSWORD", "admin")
+
+#login
+def login():
+    response = requests.post(f"{BASE_URL}/login/signin", json={
+        "username": USERNAME,
+        "password": PASSWORD
+    })
+    return response.json()["token"]
+
+#get metamodel
+def getMetaModel():
+    #login
+    token = login()
+    #resquest all sceneTypes (metamodels)
+    response = requests.get(f"{BASE_URL}/metamodel/sceneTypes", headers={"Authorization": f"Bearer {token}"})
+    metamodels = response.json()
+    return metamodels
 
 #----------------------------------Resolve Petri Net and its components----------------------------
 def getPetriNetUUID():
-
-
-
-def getTransitionUUID():
+    #resquest all sceneTypes (metamodels)
+    metamodels = getMetaModel()
     
+    #search for Petri Net
+    petriNet = next(m for m in metamodels if m["name"] == "Petri Net")
+    petriNetUUID = petriNet["uuid"]
 
+    #get classes UUIDs
+    placeUUID = next(c["uuid"] for c in petriNet["classes"] if c["name"] == "Place")
+    transitionUUID = next(c["uuid"] for c in petriNet["classes"] if c["name"] == "Transition")
+    arcUUID = next(rc["uuid"] for rc in petriNet["relationclasses"] if rc["name"] == "Arc")
 
-def getPlacesUUID():
-    
-
-
-def getMarkingUUID():
-    
-
+    return{
+        "metamodel": petriNetUUID,
+        "place": placeUUID,
+        "transition": transitionUUID,
+        "arc": arcUUID
+    }
 
 #----------------------------------Resolve BPMN and its components----------------------------
 def getBPMNUUID():
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from helpers.validators import validate_event_log
 from helpers.parser import parse_event_log
 from algorithms.router import execute_algorithm
+from helpers.metamodel_resolver import getPetriNetUUIDs
 
 app = FastAPI()
 
@@ -17,3 +18,9 @@ async def run(algorithm: str, file: UploadFile = File(...)):
     event_log = parse_event_log(file)
     result = execute_algorithm(algorithm, event_log)
     return result
+
+#-------------------------------Testing routes------------------------------------
+@app.get("/test-uuids")
+async def test_uuids():
+    from helpers.metamodel_resolver import getPetriNetUUIDs
+    return getPetriNetUUIDs()
