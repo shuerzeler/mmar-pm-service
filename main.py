320fd86af6e327f8f5811ddf781b58909f875d70
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-06-11 15:29:45 +0000

    correction

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from helpers.validators import validate_event_log
 from helpers.parser import parse_event_log
 from algorithms.router import execute_algorithm
-from helpers.uuid_resolver import getPetriNetUUIDs
+from helpers.uuid_resolver import getPetriNetUUID
 
 app = FastAPI()
 
@@ -22,5 +22,4 @@ async def run(algorithm: str, file: UploadFile = File(...)):
 #-------------------------------Testing routes------------------------------------
 @app.get("/test-uuids")
 async def test_uuids():
-    from helpers.metamodel_resolver import getPetriNetUUIDs
     return getPetriNetUUIDs()
