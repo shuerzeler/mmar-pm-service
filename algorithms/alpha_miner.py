229d4d50e110218e45bd92104162267e12d6ab31
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-06-26 13:22:54 +0000

    clean up alphaminer and add exporter call to inductive miner

--- algorithms/alpha_miner.py
+++ algorithms/alpha_miner.py
@@ -1,19 +1,12 @@
-from fastapi import HTTPException
 from pm4py.objects.log.obj import EventLog
 from pm4py.objects.petri_net.obj import PetriNet, Marking
 from pm4py.algo.discovery.alpha import algorithm as alpha_miner_algorithm
 from exporters.petri_net import createPetriNet
-import json
 
 #alpha miner can be executed as classic or plus version; returns a petri net, marking and final_marking
 #reference pm4py package: https://pm4py-source.readthedocs.io/en/stable/pm4py.algo.discovery.alpha.html last visited 11.06.26
 
 def alpha_miner(eventlog: EventLog):
-    #initalize variables for return
-    net: PetriNet 
-    im: Marking
-    fm: Marking
-
     #apply alpha algorithm
     net, im, fm = alpha_miner_algorithm.apply(eventlog)
 
--- algorithms/inductive_miner.py
+++ algorithms/inductive_miner.py
@@ -1,18 +1,14 @@
-from fastapi import UploadFile, HTTPException
 from pm4py.objects.log.obj import EventLog
 from pm4py.objects.petri_net.obj import PetriNet, Marking
-from pm4py.algo.discovery.inductive import algorithm as inductive_miner_algorithm
-from pm4py.objects.conversion.process_tree import converter as pt_converter
-
+from pm4py import discover_petri_net_inductive
+from exporters.petri_net import createPetriNet
 
 def inductive_miner(eventlog: EventLog):
-    process_tree = inductive_miner_algorithm.apply(eventlog)
-    net, im, fm = pt_converter.apply(process_tree)
+    #apply inductive algorithm
+    net, im, fm = discover_petri_net_inductive(eventlog)
+
+    #call exporter to save net to database
+    result = createPetriNet(net, im, fm)
 
     #just return sth to test
-    return {
-        "algorithm": "inductive_miner",
-        "places": len(net.places),
-        "transitions": len(net.transitions),
-        "arcs": len(net.arcs)
-    }
+    return result
