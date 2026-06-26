1c99d12a572e3d4bd136dc3f47aba44d6a9df2ad
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-06-26 14:30:13 +0000

    modify parser for xes files

--- helpers/parser.py
+++ helpers/parser.py
@@ -3,6 +3,8 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.conversion.log import converter as log_converter
 import pandas as pd 
 import io
+import tempfile
+import os
 
 REQUIRED_CSV_COLUMNS = ["case:concept:name", "concept:name", "time:timestamp"]
 
@@ -21,7 +23,11 @@ def parse_event_log(file: UploadFile):
         event_log = log_converter.apply(df)
         return event_log   
     elif filename.endswith(".xes"):
-        event_log = xes_importer.apply(io.BytesIO(contents))
+        with tempfile.NamedTemporaryFile(suffix=".xes", delete=False) as tmp:
+            tmp.write(contents)
+            tmp_path = tmp.name
+        event_log = xes_importer.apply(tmp_path)
+        os.unlink(tmp_path)
         return event_log
     else:
         raise HTTPException(
