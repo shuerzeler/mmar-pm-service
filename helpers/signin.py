cab8ee95e5c4201ecd7873756bad6a80a87a96d0
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-07-07 14:11:20 +0000

    add default login as fallback -> default login is already publicly in docker installation read.me

--- helpers/signin.py
+++ helpers/signin.py
@@ -5,8 +5,8 @@ import os
 #server URL for backend serverand login
 BASE_URL = "http://mmar-server:8000"
 load_dotenv()
-USERNAME = os.getenv("MMAR_USERNAME")
-PASSWORD = os.getenv("MMAR_PASSWORD")
+USERNAME = os.getenv("MMAR_USERNAME", "admin")
+PASSWORD = os.getenv("MMAR_PASSWORD", "admin")
 
 #login
 def login():
